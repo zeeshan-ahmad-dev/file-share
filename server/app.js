5cbf512d0370f98ccaa48605e61bc28d8bbb3a3e
zeeshan-ahmad-dev <za5743759@gmail.com>
2025-09-19 12:19:31 +0500

    chors

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://file-share-z.vercel.app/"],
+    origin: ["http://localhost:5173", "https://file-share-z.vercel.app"],
     methods: ["GET", "POST"]
   },
 });
@@ -34,7 +34,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://file-share-z.vercel.app/"],
+  origin: ["http://localhost:5173", "https://file-share-z.vercel.app"],
   credentials: true
 }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
